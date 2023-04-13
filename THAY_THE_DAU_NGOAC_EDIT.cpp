4845cdb18b3db912f09c1b5e06b4486c2c486fcc
qhoa25623 <qhoa25623@outlook.com>
2023-04-14 00:29:56 +0700

    SO BUOC DI CHUYEN IT NHAT

--- THAY_THE_DAU_NGOAC_EDIT.cpp
+++ THAY_THE_DAU_NGOAC_EDIT.cpp
@@ -11,15 +11,14 @@ int main() {
 	while (t--) {
 		string s;
 		cin >> s;
-		stack<int>st; //stack nay luu vi tri cac dau sai
-		int a[1005]; //a[i] = 1: Ngoac vi tri i dung/vi tri do khong la dau ngoac, a[i] = 0: Ngoac vi tri i sai
+		stack<int>st;
+		int a[1005]; 
 		for (int i = 0; i < s.size(); i++) a[i] = 1;
 		for (int i = 0; i < s.size(); i++) {
 			if (s[i] == '(') {
 				st.push(i);
 			} else if (s[i] == ')') {
 				if (!st.empty() && s[st.top()] == '(') {
-					//Danh dau dau mo 0 dau dong la 1
 					a[i] = 1; a[st.top()] = 0;
 					st.pop();
 				} else {
