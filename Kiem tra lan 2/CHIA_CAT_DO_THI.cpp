57052829db4afc1c28df080490762f8366c9cb5c
qhoa25623 <qhoa25623@outlook.com>
2023-04-13 20:22:09 +0700

    Update Code

---
+++ Kiem tra lan 2/CHIA_CAT_DO_THI.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int t, n, m;
+vector<int> adj[1000001];
+int visited[1000001];
+
+void DFS(int u)
+{
+	visited[u] = 1;
+	for (auto x : adj[u])
+	{
+		if (!visited[x])
+			DFS(x);
+	}
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	cin >> t;
+	while (t--) {
+		cin >> n >> m;
+		for (int i = 0; i < m; i++)
+		{
+			int x, y;
+			cin >> x >> y;
+			adj[x].push_back(y);
+			adj[y].push_back(x);
+		}
+		int cnt = 0;
+		for (int i = 1; i <= n; i++)
+		{
+			if (!visited[i])
+			{
+				cnt++;
+				DFS(i);
+			}
+		}
+		if (cnt == 0) {
+			cout << 0 << endl;
+		} else {
+			int ans = 0;
+			for (int i = 1; i <= n; i++)
+			{
+				visited[i] = 1;
+				int dem = 0;
+				for (int j = 1; j <= n; j++)
+				{
+					if (!visited[j])
+					{
+						dem++;
+						DFS(j);
+					}
+				}
+				memset(visited, false, sizeof(visited));
+				if (dem > cnt)
+					ans = max(ans, dem);
+			}
+			cout << ans << endl;
+		}
+		memset(visited, false, sizeof(visited));
+	}
+	return 0;
+}
